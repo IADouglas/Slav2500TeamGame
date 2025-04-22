82cca8e7c5e40c12a85a51d69bf255da39108b38
IADouglas <jvs3yk@virginia.edu>
2025-04-22 18:20:33 -0400

    fsdai

--- FollowScript.cs
+++ FollowScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class FollowScript : MonoBehaviour
 {
     public GameObject player;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,5 +15,9 @@ public class FollowScript : MonoBehaviour
     void Update()
     {
         //transform.position = (Vector2)player.transform.position;//+ ( Vector2.up * 2f); 
+        Vector3 pos = player.transform.position + new Vector3(7.77f,1.79f,-10f);
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, pos, 0.125f);
+        transform.position = smoothedPos;
+
     }
 }
